346671273676ff7926042e36f361e9a8974eebbb
smittyfest <smittyfest@users.noreply.github.com>
2016-02-09 18:56:30 -0600

    Update Problem4.java

--- Level-Zero/Problem4.java
+++ Level-Zero/Problem4.java
@@ -8,6 +8,8 @@ package euler.level.zero;
  * 2-digit numbers is 9009 = 91 × 99.
  *
  * Find the largest palindrome made from the product of two 3-digit numbers.
+ * 
+ * @author smittyfest
  */
 public class Problem4 {
   public static void main(String[] args) {
