40df2193385552362618c4fc8ab650db7a64886e
smittyfest <smittyfest@users.noreply.github.com>
2016-02-19 13:22:59 -0600

    Create Problem28.java

---
+++ Level-01/Problem28.java
@@ -0,0 +1,30 @@
+package euler.level.one;
+
+/**
+ * Number spiral diagonals
+ *
+ * Starting with the number 1 and moving to the right in a clockwise direction,
+ * a 5 by 5 spiral is formed as follows:
+ *
+ * (21)  22  23  24 (25)
+ * 20   (7)  8  (9)  10
+ * 19    6  (1)  2   11
+ * 18   (5)  4  (3)  12
+ * (17) 16  15   14 (13)
+ *
+ * It can be verified that the sum of the numbers on the diagonals is 101.
+ *
+ * What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+ *
+ * @author smittyfest
+ */
+public class Problem28 {
+  public static void main(String[] args) {
+    int limit = 1001;
+    long sum = 1;
+    for (int i = 3; i <= limit; i += 2) {
+      sum += 4 * i * i - 6 * (i - 1);
+    }
+    System.out.println(sum);
+  }
+}
